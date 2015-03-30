e5308fd69e41eb8c97e89b322db4e4d624e65099
Brent Arata <brent.arata@gmail.com>
2015-03-30 16:17:17 -0700

    Problem one solved

---
+++ problem_one/MultiplesOfThreeAndFive.java
@@ -0,0 +1,15 @@
+public class MultiplesOfThreeAndFive {
+	public static int Get(int n) {
+		int sum = 0;
+		for (int i = 0; i < n; ++i) {
+			if (i % 3 == 0) {
+				sum += i;
+				
+			}
+			else if (i % 5 == 0) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
+}
